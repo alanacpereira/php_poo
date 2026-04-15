7f83a68bd3147ae3c5f974d4c62a2d8ac01e2792
alanacpereira <alana.c.pereira@aluno.senai.br>
2026-04-15 16:55:14 -0300

    carro
    ativ.

---
+++ carro.php
@@ -0,0 +1,33 @@
+<?php
+class Carro {
+    // RISCO: Atributo público permite valores ilegais
+    public $modelo;
+    private $velocidade;
+
+    public function __construct($modelo, $velocidade) {
+        $this->modelo = $modelo;
+        $this->velocidade = $velocidade;
+    }
+
+    public function getVelocidade() {
+        return $this->velocidade;
+    }
+
+     public function setVelocidade($novaVelocidade) {
+        if($novaVelocidade >= 0 && $novaVelocidade <= 200) {
+            $this->velocidade = $novaVelocidade;
+        } else {
+            echo "Velocidade inválida: $novaVelocidade km/h.Deve ser entre 0 e 200 km/h.";
+        }
+    }
+}
+
+// --- TESTE DO VEÍCULO ---
+$meuCarro = new Carro("Senai-Mobile", 0);
+
+// O desastre: alteração direta sem validação
+$meuCarro->setVelocidade(5000); // Velocidade de foguete?
+$meuCarro->setVelocidade(-60);   // Carro andando no tempo?
+
+echo "Modelo: " . $meuCarro->modelo . "<br>";
+echo "Velocidade atual: " . $meuCarro->getVelocidade() . " km/h";
