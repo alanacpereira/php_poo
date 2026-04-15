5a923cd08ac3b645ebc651c0a50049f51e6bd7a2
alanacpereira <alana.c.pereira@aluno.senai.br>
2026-04-15 11:16:29 -0300

    exemplo 17
    usando public, protected, private

---
+++ exemplo17.php
@@ -0,0 +1,41 @@
+<?php 
+
+class Visibilidade { 
+
+//Atributos - Proriedades
+    public $varPublic;
+    protected $varProtected;
+    private $varPrivate; 
+    
+    public function __construct($varPublic, $varProtected, $varPrivate)
+    {
+        $this->varPublic = $varPublic;
+        $this->varProtected = $varProtected;
+        $this->varPrivate = $varPrivate;
+    }
+    
+    public function publicFunc()
+    {
+        echo "Método Público <br>";
+    }
+
+    public function protectedFunc()
+    {
+        echo "Método Protegido <br>";
+    }
+
+    public function privateFunc()
+    {
+        echo "Método Privado <br>";
+    }
+}
+
+$teste = new Visibilidade(1,2,3);
+echo "Atributo Public = $teste->varPublic <br>";
+// echo "Atributo Protected = $teste->varProtected <br>";
+// echo "Atributo Private = $teste->varPrivate <br>";
+
+echo "<br>";
+$teste->publicFunc();
+$teste->protectedFunc();
+$teste->privateFunc();
